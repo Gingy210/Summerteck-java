ed9fa98d2d2dcb89fa1bd6d17756215416b0a65a
Gingy210 <harrisonfregan@gmail.com>
2025-06-29 20:54:14 +0000

    Finish equalateral triangle

--- Shapes.java
+++ Shapes.java
@@ -1,1 +1,21 @@
-
+ import java.util.Scanner;
+public class Shapes{
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+   int o=scan.nextInt();
+        for(int i=0;i<o;i++){
+            for(int z=0;z<i+1;z++){
+System.out.print("x");
+       }
+       System.out.println("");
+    
+}
+for(int c=0;c<3;c++){
+    for( int b=2;b<c-1;b--){
+       for(int d= )
+        System.out.print(" ");
+    }
+    
+}
+}
+}
