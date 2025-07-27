f88c691b03cb58ffcdb9552cb7ac57db0f3c6772
Gingy210 <harrisonfregan@gmail.com>
2025-07-27 20:03:12 +0000

    gffg

---
+++ Conect4.java
@@ -0,0 +1,116 @@
+import java.util.Scanner;
+public class Conect4{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+String[][] Board  = new String[6][7];
+for(int i=0;i<6;i++){
+    for(int k=0;k<7;k++){
+        Board [i][k]="__";
+        System.out.print( Board[i][k]+"     ");
+    }
+    System.out.println("");
+}
+String turn = "RED";
+int j=1;
+
+while(j<43){
+System.out.println(turn + "'s Turn. penk a colomn");
+int X=scan.nextInt(); 
+while(X>7||!Board [0][X].equals("__")){
+   System.out.println("Use Valid Numbers");
+    X=scan.nextInt();
+}
+for(int q=5;q>=0;q--){
+    if(Board[q][X]!="🥵" && Board[q][X]!="🥶"){
+if(turn=="RED"){
+        Board[q][X]="🥵";
+}
+else{
+    Board[q][X]="🥶";
+}
+q=0;
+    }
+ }
+ for(int i=0;i<6;i++){
+    for(int k=0;k<7;k++){
+        System.out.print( Board[i][k]+"     ");
+    }
+    System.out.println("");
+ }
+ int p=0;
+ int h=0;
+for(int e=0;e<=5;e++){
+    for(int x=0;x<=3;x++){
+   
+    if(Board [e][x]=="🥵" && Board [e][x+1]=="🥵" && Board [e][x+2]=="🥵" && Board [e][x+3]=="🥵"){
+        System.out.println("Red wins");
+    System.exit(0);
+    }
+    if(Board [e][x]=="🥶" && Board [e][x+1]=="🥶" && Board [e][x+2]=="🥶" && Board [e][x+3]=="🥶"){
+        System.out.println("Blue wins");
+    System.exit(0);
+    }
+    
+
+}
+}
+for(int Row=0;Row<=2;Row++){
+    for(int x=0;x<=3;x++){
+   
+    if(Board [Row][x]=="🥵" && Board [Row+1][x+1]=="🥵" && Board [Row+2][x+2]=="🥵" && Board [Row+3][x+3]=="🥵"){
+        System.out.println("Red wins");
+    System.exit(0);
+    }
+    if(Board [Row][x]=="🥶" && Board [Row+1][x+1]=="🥶" && Board [Row+2][x+2]=="🥶" && Board [Row+3][x+3]=="🥶"){
+        System.out.println("Blue wins");
+    System.exit(0);
+    }
+    
+
+}
+}
+for(int Row=0;Row<=2;Row++){
+    for(int x=3;x<=6;x++){
+   
+    if(Board [Row][x]=="🥵" && Board [Row+1][x-1]=="🥵" && Board [Row+2][x-2]=="🥵" && Board [Row+3][x-3]=="🥵"){
+        System.out.println("Red wins");
+    System.exit(0);
+    }
+    if(Board [Row][x]=="🥶" && Board [Row+1][x-1]=="🥶" && Board [Row+2][x-2]=="🥶" && Board [Row+3][x-3]=="🥶"){
+        System.out.println("Blue wins");
+    System.exit(0);
+    }
+    
+
+}
+}
+for(int Row=0;Row<=2;Row++){
+    for(int x=0;x<=6;x++){
+   
+    if(Board [Row][x]=="🥵" && Board [Row+1][x]=="🥵" && Board [Row+2][x]=="🥵" && Board [Row+3][x]=="🥵"){
+        System.out.println("Red wins");
+    System.exit(0);
+    }
+    if(Board [Row][x]=="🥶" && Board [Row+1][x]=="🥶" && Board [Row+2][x]=="🥶" && Board [Row+3][x]=="🥶"){
+        System.out.println("Blue wins");
+    System.exit(0);
+    }
+    
+
+}
+}
+if(turn.equals("RED")){
+    turn="Blue";
+    j++;
+}
+else{
+    turn="RED";
+    j++;
+}
+
+    }
+    System.out.println("Draw");
+    }
+}
+    
+
--- Ticktactoe.java
+++ Ticktactoe.java
@@ -10,12 +10,99 @@ for(int i=0;i<3;i++){
     }
     System.out.println("");
 }
+int j=1;
+int count = 0;
+while(j<10){
+
+if(j%2==1 && j<10){
 System.out.println("X's Turn. Y coordinant then X coordinant 0,1,2");
 int X=scan.nextInt();
 int Y=scan.nextInt();
-while(x>2||y>2){
+while(X>2||Y>2||Board [Y][X]!="_"){
    System.out.println("Use Valid Numbers (0,1,2)");
+    X=scan.nextInt();
+     Y=scan.nextInt();
 }
     Board [Y][X]="X"; 
+   for(int i=0;i<3;i++){
+    for(int k=0;k<3;k++){
+        System.out.print(Board[i][k]);
     }
+System.out.println("");
+} 
+checkboard(Board);
+j++;
+if(j%2==0 && j<9){
+    System.out.println("O's Turn. Y coordinant then X coordinant 0,1,2");
+    X=scan.nextInt();
+     Y=scan.nextInt();
+    while(X>2||Y>2||Board [Y][X]!="_"){
+   System.out.println("Use Valid Numbers (0,1,2)");
+    X=scan.nextInt();
+     Y=scan.nextInt();
+}
+Board [Y][X]="O"; 
+for(int i=0;i<3;i++){
+    for(int k=0;k<3;k++){
+        System.out.print(Board[i][k]);
+    }
+System.out.println("");
+}
+checkboard(Board);
+j++;
+}
+
+}
+    }
+    System.out.print("Tie, no one wins");
+    System.exit(0);
+}
+
+public static void checkboard (String[][] Board){
+for(int w=0;w<3;w++){
+if(Board [w][0].equals("X") && Board [w][1].equals("X") && Board [w][2].equals("X")){
+    System.out.print("X wins");
+    System.exit(0);
+}
+
+    }
+    for(int w=0;w<3;w++){
+if(Board [0][w].equals("X") && Board [1][w].equals("X") && Board [2][w].equals("X")){
+    System.out.print("X wins");
+    System.exit(0);
+}
+
+    }
+    for(int w=0;w<3;w++){
+if(Board [w][0].equals("O") && Board [w][1].equals("O") && Board [w][2].equals("O")){
+    System.out.print("O wins");
+    System.exit(0);
+}
+
+    }
+    for(int w=0;w<3;w++){
+if(Board [0][w].equals("O") && Board [1][w].equals("O") && Board [2][w].equals("O")){
+    System.out.print("O wins");
+    System.exit(0);
+}
+
+    }
+    if(Board [0][0].equals("X") && Board [1][1].equals("X") && Board [2][2].equals("X")){
+        System.out.print("X wins");
+        System.exit(0);
+    }
+   if(Board [0][2].equals("X") && Board [1][1].equals("X") && Board [2][0].equals("X")){
+        System.out.print("X wins");
+        System.exit(0);
+    }
+if(Board [0][0].equals("O") && Board [1][1].equals("O") && Board [2][2].equals("O")){
+        System.out.print("O wins");
+        System.exit(0);
+    }
+   if(Board [0][2].equals("O") && Board [1][1].equals("O") && Board [2][0].equals("O")){
+        System.out.print("O wins");
+        System.exit(0);
+    }
+}
+    
 }
