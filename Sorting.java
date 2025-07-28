8f96b923178f73114f8d7defb50c8031cb20554a
Gingy210 <harrisonfregan@gmail.com>
2025-07-28 15:17:02 +0000

    Printing array

---
+++ Sorting.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+public class Sorting {
+     public static void main(String[] a){
+       int[] b={4, 1, 5, 3, 2};
+       array(b);
+       
+       print(b);
+     }
+     /* 
+     public static int[] Sortingmain(int[] a){
+Random r=new Random();
+
+     }*/
+     public static void swap(int[] a,int x1, int x2){
+        
+     }
+     public static void array(int[] a){
+        System.out.println(a.length);
+     }
+     public static void print(int[] a){
+        for(int g=0;g<5;g++){
+            System.out.println(a[g]);
+        }
+        
+     }
+}
