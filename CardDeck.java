696a56ca525eb9a0200f1826f22a1818013dea8e
Gingy210 <harrisonfregan@gmail.com>
2025-07-31 14:01:15 +0000

    Drawing

--- CardDeck.java
+++ CardDeck.java
@@ -1,34 +1,76 @@
+import java.util.Random;
+
 public class CardDeck {
+    private Random r;
    private Card[]Deck;
+   private int draw;
 public CardDeck(){
-    for(int index=0;index<13;index++){
+    Deck=new Card[52];
+   r=new Random();
+   draw=51;
         for(int x=1;x<=13;x++){
-        Deck[index]=new Card(x, "Spades");
-    }
+        Deck[x-1]=new Card(x, "Spades");
     }
     
-     for(int index=0;index<13;index++){
+    
+ 
         for(int x=1;x<=13;x++){
-        Deck[index]=new Card(x, "Hearts");
+        Deck[x+12]=new Card(x, "Hearts");
      }
-    }
- for(int index=0;index<13;index++){
+    
+
     for(int x=1;x<=13;x++){
-        Deck[index]=new Card(x, "Diamonds");
+        Deck[x+25]=new Card(x, "Diamonds");
     }
- }
-    for(int index=0;index<13;index++){
+ 
+  
     for(int x=1;x<=13;x++){
-        Deck[index]=new Card(x, "Clubs");
+        Deck[x+38]=new Card(x, "Clubs");
     }
- }
+ 
  
 }
 public void print(){
-for(int x=0;x<13;x++){
-    System.out.println(Deck);
+for(int x=0;x<52;x++){
+    System.out.println(Deck[x]);
 }
  }
+
+ public int random(int min, int max) {
+      int s = r.nextInt(max - min + 1);
+      return s + min;
+   }
+ public void shuffle() {
+
+      for (int o = 0; o < 52; o++) {
+         int u = random(o, 51);
+         swap(u, o);
+
+      }
+
+   }
+   private void swap(int x1, int x2) {
+
+      Card q = Deck[x1];
+      Deck[x1] = Deck[x2];
+      Deck[x2] = q;
+
+   }
+   public Card drawing(){
+    int d=draw;
+    draw--;
+return Deck[d];
+
+   }
+   public boolean isEmpty(){
+if (draw<=-1){
+    return true;
+}
+else{
+    return false;
+}
+   }
+
 }
 
 
--- CardTester.java
+++ CardTester.java
@@ -1,6 +1,6 @@
 public class CardTester {
     public static void main(String[]arrgs){
-        
+        CardDeck DeckCard=new CardDeck();
 Card card=new Card(5, "Spades");
 System.out.println(card.valueGet());
 System.out.println(card.suitGet());
@@ -8,7 +8,13 @@ System.out.println(card);
 card.setValue(4);
 card.setSuit("Politoeds");
 System.out.println(card);
-CardDeck.print();
+DeckCard.shuffle();
+DeckCard.print();
+for(int draw=0;DeckCard.isEmpty()==false;draw++){
+    Card d= DeckCard.drawing();
+System.out.println(d);
+}
+
 }
     
 }
